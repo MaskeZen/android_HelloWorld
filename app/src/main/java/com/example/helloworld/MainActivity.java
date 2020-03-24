ed0568fa9d6d91ca67bbd5f87c7e5de4b5227cfd
Diego Mascheroni <maskeee@gmail.com>
2020-03-23 22:49:21 -0300

    01.1: Android Studio and Hello World

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "Hello World!");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
